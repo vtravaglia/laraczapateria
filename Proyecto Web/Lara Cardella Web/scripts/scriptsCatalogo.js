5530ed63b990e0916b29e90c17d4d196ae213d64
nicofetter@gmail.com <nicofetter@gmail.com@cc4894a8-a914-3040-d441-f73a1a60528e>
2011-12-04 23:26:44 +0000

    Catalogo dinamico listo, me falta agregar descripcion a las fotos

--- Proyecto Web/Lara Cardella Web/scripts/scriptsCatalogo.js
+++ Proyecto Web/Lara Cardella Web/scripts/scriptsCatalogo.js
@@ -1,7 +1,6 @@
 ﻿$(document).ready(function() {
     //Antes de los eventos de los botones debo cargar los catalogos
-    
-    //CATALOGO ZAPATOS  OTO-INV       
+    $("body").append( '<scr' + 'ipt id="newScript" type="text/javascript" src=" yoxview/yoxview-nojquery.js "><\/scr' + 'ipt>' ) ;    
     var zapatosOtoInv=$("#ctl00_contenidoPlaceHolder_listaCalzadosOtoInv").val();
     var arrayZapatosOtoInv=zapatosOtoInv.split(",");
     
@@ -17,46 +16,89 @@
     var accesoriosPriVer=$("#ctl00_contenidoPlaceHolder_listaAccesoriosPriVer").val();
     var arrayAccesoriosPriVer=accesoriosPriVer.split(",");
     
+    
+    //COMPLETAR ZAPATOS OTO-INV
     //A la fila 1 del catalogo 
     var cantidadPorFila=parseInt(arrayZapatosOtoInv.length/2);
     for (var i=0; i<cantidadPorFila; i++) {
         var arrayImagen=arrayZapatosOtoInv[i].split(":");
-        $("#mycarousel").append( '<li ><a class="yoxview" href="'+arrayImagen[1]+'"><img src="'+arrayImagen[0]+'" alt="" /></a></li>' ) ;
-    }         
-                
+        $("#mycarouselCalOtoInv1").append( '<li ><a class="yoxview" href="'+arrayImagen[1]+'"><img src="'+arrayImagen[0]+'" /></a></li>' ) ;
+    }                    
     //A la fila 2 del catalogo 
     for (var i=cantidadPorFila; i<arrayZapatosOtoInv.length; i++) {
         var arrayImagen=arrayZapatosOtoInv[i].split(":");
-        $("#mycarousel2").append( '<li ><a class="yoxview" href="'+arrayImagen[1]+'"><img src="'+arrayImagen[0]+'" alt="" /></a></li>' ) ;
+        $("#mycarouselCalOtoInv2").append( '<li ><a class="yoxview" href="'+arrayImagen[1]+'"><img src="'+arrayImagen[0]+'"/></a></li>' ) ;
     }
-           
+     
+    //CATALOGO ZAPATOS  OTO-INV       
+    //COMPLETAR ACCESORIOS OTO-INV
+    //A la fila 1 del catalogo 
+    var cantidadPorFila=parseInt((arrayAccesoriosOtoInv.length)/2);
+    for (var i=0; i<cantidadPorFila; i++) {
+        var arrayImagen=arrayAccesoriosOtoInv[i].split(":");
+        $("#mycarouselAccPriVer1").append( '<li ><a class="yoxview" href="'+arrayImagen[1]+'"><img src="'+arrayImagen[0]+'" /></a></li>' ) ;
+    }                    
+    //A la fila 2 del catalogo 
+    for (var i=cantidadPorFila; i<arrayAccesoriosOtoInv.length; i++) {
+        var arrayImagen=arrayAccesoriosOtoInv[i].split(":");
+        $("#mycarouselAccPriVer2").append( '<li ><a class="yoxview" href="'+arrayImagen[1]+'"><img src="'+arrayImagen[0]+'"/></a></li>' ) ;
+    }
+        
+      
+         
+     /******EVENTOS***********/
+     //Calzados Primavera-Verano
+     $("#calzadosPriVer").click(function(){
+            if ($("#catalogoCalPriVer").is(":visible")){
+                $("#catalogoCalPriVer").fadeOut('normal').addClass('hidden'); 
+            }else{
+                $("#catalogoCalOtoInv").fadeOut('normal').addClass('hidden'); 
+                $("#catalogoAccPriVer").fadeOut('normal').addClass('hidden');
+                $("#catalogoAccOtoInv").fadeOut('normal').addClass('hidden');
+                
+                $("#catalogoCalPriVer").fadeIn('normal').removeClass('hidden'); 
+            }         
+     });
+     
      //Calzados Otoño-Invierno
      $("#calzadosOtoInv").click(function(){
-            if ($("#contenidoIzq").width()!=10){
-                $("#contenidoIzq").animate({width: 10}, 'slow');
-                $("div:hidden").show("slow");
-                $("body").append( '<scr' + 'ipt id="newScript" type="text/javascript" src=" yoxview/yoxview-nojquery.js "><\/scr' + 'ipt>' ) ;
+            if ($("#catalogoCalOtoInv").is(":visible")){
+                $("#catalogoCalOtoInv").fadeOut('normal').addClass('hidden'); 
+            }else{
+                $("#catalogoCalPriVer").fadeOut('normal').addClass('hidden'); 
+                $("#catalogoAccPriVer").fadeOut('normal').addClass('hidden');
+                $("#catalogoAccOtoInv").fadeOut('normal').addClass('hidden');
+                
+                $("#catalogoCalOtoInv").fadeIn('normal').removeClass('hidden'); 
             }         
      });
     
-    //Calzados Primavera-Verano
-     $("#calzadosPriVer").click(function(){
-           /* $("div:hidden").hide("slow");  
-            
-            $("#contenidoIzq").animate({width: 10}, 'slow');
-            $("div:hidden").show("slow");
-            $("body").append( '<scr' + 'ipt id="newScript" type="text/javascript" src=" yoxview/yoxview-nojquery.js "><\/scr' + 'ipt>' ) ;  */      
+    //Accesorios Primavera-Verano
+     $("#accesoriosPriVer").click(function(){
+           if ($("#catalogoAccPriVer").is(":visible")){
+                $("#catalogoAccPriVer").fadeOut('normal').addClass('hidden'); 
+            }else{
+                $("#catalogoCalPriVer").fadeOut('normal').addClass('hidden'); 
+                $("#catalogoCalOtoInv").fadeOut('normal').addClass('hidden');
+                $("#catalogoAccOtoInv").fadeOut('normal').addClass('hidden');
+                 
+                $("#catalogoAccPriVer").fadeIn('normal').removeClass('hidden'); 
+            }
      });
      
      //Accesorios Otoño-Invierno
      $("#accesoriosOtoInv").click(function(){
-                      
+            if ($("#catalogoAccOtoInv").is(":visible")){
+                $("#catalogoAccOtoInv").fadeOut('normal').addClass('hidden'); 
+            }else{
+                $("#catalogoCalPriVer").fadeOut('normal').addClass('hidden'); 
+                $("#catalogoCalOtoInv").fadeOut('normal').addClass('hidden');
+                $("#catalogoAccPriVer").fadeOut('normal').addClass('hidden');
+                 
+                $("#catalogoAccOtoInv").fadeIn('normal').removeClass('hidden'); 
+            }       
      });
      
-     //Accesorios Primavera-Verano
-     $("#accesoriosPriVer").click(function(){
-                      
-     });
      
      //Expandir imagenes del catalogo
      $(".yoxview").click(function(){
@@ -68,6 +110,14 @@
      });
     
     //slider images
-    $('#mycarousel').jcarousel();
-    $('#mycarousel2').jcarousel();
+    $('#mycarouselCalPriVer1').jcarousel();
+    $('#mycarouselCalPriVer2').jcarousel();
+    $('#mycarouselCalOtoInv1').jcarousel();
+    $('#mycarouselCalOtoInv2').jcarousel();
+    
+    $('#mycarouselAccPriVer1').jcarousel();
+    $('#mycarouselAccPriVer2').jcarousel();
+    $('#mycarouselAccOtoInv1').jcarousel();
+    $('#mycarouselAccOtoInv2').jcarousel();
+    
 });
